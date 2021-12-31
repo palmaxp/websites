8659b2b48d777326b758878c052e095378891eb7
palma <78461439+palmaxp@users.noreply.github.com>
2021-12-31 11:38:41 -0300

    animations

--- Desired42/script.js
+++ Desired42/script.js
@@ -4,3 +4,12 @@ const toggleMenu = () => {
   nav.classList.toggle("active");
 };
 btnMobile.addEventListener("click", toggleMenu);
+
+window.sr = ScrollReveal({ reset: true  });
+sr.reveal('h1', { duration: 2000 });
+sr.reveal('h2', { duration: 2000 });
+sr.reveal('h3', { duration: 2000 });
+sr.reveal('img', { duration: 2000, delay: '500' });
+sr.reveal('.brands-content-item', { duration: 500, distance: '30px'});
+sr.reveal('.brands-content-item img', { duration: 1000, distance: '30px', delay: 500});
+sr.reveal('i', { duration: 1000, distance: '60px', delay: 1000});
