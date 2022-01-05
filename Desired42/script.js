597d1e72ceaff51d102a5d1b2c4114cc132af9eb
palma <78461439+palmaxp@users.noreply.github.com>
2022-01-04 21:52:33 -0300

    animation

--- Desired42/script.js
+++ Desired42/script.js
@@ -1,15 +1,22 @@
-const btnMobile = document.querySelector("#btn-mobile");
+const btnMobile = document.querySelector('#btn-mobile');
 const toggleMenu = () => {
-  const nav = document.querySelector("nav");
-  nav.classList.toggle("active");
+	const nav = document.querySelector('nav');
+	nav.classList.toggle('active');
 };
-btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener('click', toggleMenu);
 
-window.sr = ScrollReveal({ reset: true  });
+window.sr = ScrollReveal({ reset: false });
 sr.reveal('h1', { duration: 2000 });
 sr.reveal('h2', { duration: 2000 });
-sr.reveal('h3', { duration: 2000 });
-sr.reveal('img', { duration: 2000, delay: '500' });
-sr.reveal('.brands-content-item', { duration: 500, distance: '30px'});
-sr.reveal('.brands-content-item img', { duration: 1000, distance: '30px', delay: 500});
-sr.reveal('i', { duration: 1000, distance: '60px', delay: 1000});
+sr.reveal('.brands-content-item', {
+	duration: 2500,
+	distance: '30px',
+	reset: true,
+});
+sr.reveal('.brands-content-item img', {
+	duration: 2000,
+	distance: '30px',
+	delay: 700,
+	reset: true,
+});
+sr.reveal('i', { duration: 1000, distance: '60px', delay: 1000 });
